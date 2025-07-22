8ea0f646f6e1d5e6a925df9f1d05845cfa39bd7a
Anup-Gelal <anup39gelal@gmail.com>
2025-07-22 10:53:48 +0545

    assets

---
+++ src/assets/index.js
@@ -0,0 +1,92 @@
+import menu from "./menu.svg";
+import close from "./close.svg";
+import logo from "./cropped-logo-slbbl.png";
+import cattle from "./cattle.jpg";
+import digitalpartnerkhalti from "./digitalpartnerkhalti.jpg";
+import membersecurityfund from "./membersecurityfund.jpg";
+import mobilefinance from "./mobilefinance.jpg";
+import remittance from "./remittance.jpg";
+import esewapayment from "./esewapayment.jpg";
+import scholarship from "./scholarship.jpg";
+import cfs from "./centrefundsaving.jpg";
+import ds from "./disastersaving.jpg";
+import dcws from "./disciplinecenterwelfaresaving.jpg";
+import fcs from "./festivalchildsaving.jpg";
+import ms from "./monthlysaving.jpg";
+import ops from "./optionalpersonalsaving.jpeg";
+import os from "./optionalsaving.jpg";
+import ps from "./pewasaving.jpg";
+import sbfs from "./swarojgarbrightfuturesaving.jpg";
+import scs from "./swarojgarchildsaving.jpg";
+import ael from "./alternativeenergyloan.jpg";
+import bgl from "./businessgrowthloan.png";
+import chl from "./centerhomeloan.jpg";
+import fel from "./foreignemploymentloan.jpg";
+import gl from "./generalloan.jpg";
+import cp from "./chairperson.jpg";
+import rp1 from "./representative.jpg";
+import rpg2 from "./repgen2.jpg";
+import rpg3 from "./repgen3.jpeg";
+import rp2 from "./representative1.jpg";
+import rpg1 from "./representativegen1.jpg";
+import idd from "./independent.jpg";
+import agm from "./agm.png";
+import ao1 from "./ao1.jpg";
+import ao2 from "./ao2.png";
+import cdh from "./cdh.jpg";
+import ceo from "./ceo.jpg";
+import hgsd from "./hgsd.jpg";
+import hhrm from "./hhrm.jpg";
+import hit from "./hit.jpg";
+import hodaf from "./hodaf.jpg";
+import hrd from "./hrd.jpg";
+import htd from "./htd.jpg";
+import o1 from "./o1.jpg";
+import o2 from "./o2.jpg";
+import o3 from "./o3.jpg";
+import o4 from "./o4.jpg";
+import staff from "./staff.png";
+import branch from "./branch.webp";
+import s11 from "./s1-1.jpg";
+import s12 from "./s1-2.jpg";
+import vac1 from "./vaccancy1.jpg";
+import bg12 from "./bg12.png";
+import arbindkumargupta from "./arbindkumargupta.jpg";
+import jeevanmanandar from "./jeevanmanandar.png";
+import nabindawadi from "./nabindawadi.png";
+import purshottamkoirala from "./purshottamkoirala.png";
+import ah from "./ah.jpg";
+
+import agm208201 from "./agm2082-01.jpg";
+import agm208202 from "./agm2082-02.jpg";
+import agm208203 from "./agm2082-03.jpg";
+import agm208204 from "./agm2082-04.jpg";
+
+
+
+import tirtharajneupane from "./tirtharajneupane.jpg";
+
+
+import backgroundimage from "./backgroundimage.avif";
+import backgroundimage1 from "./backgroundimage.webp";
+ const bg3 = "/bg3.htm";
+ const bg4 = "/bg4.htm";
+ const bg5 = "/bg5.htm";
+ const bg6 = "/bg6.htm";
+ const bg7 = "/bg7.htm";
+ const bg8 = "/bg8.htm";
+ const bg9 = "/bg9.htm";
+ const bg10 = "/bg10.htm";
+ const bg11 = "/bg11.htm";
+
+
+
+export {
+    menu,close,logo,cattle,digitalpartnerkhalti,membersecurityfund,mobilefinance,remittance,esewapayment,scholarship,
+    cfs,ds,dcws,fcs,ms,ops,os,ps,sbfs,scs,
+    ael,bgl,chl,fel,gl,ah,
+    cp,rp1,rpg2,rpg3,rp2,rpg1,idd,
+    agm,ao1,ao2,cdh,ceo,hgsd,hhrm,hit,hodaf,hrd,htd,o1,o2,o3,o4,staff,branch,
+    s11,s12,vac1,backgroundimage,backgroundimage1,bg3,bg4,bg5,bg6,bg7,bg8,bg9,bg10,bg11,bg12,arbindkumargupta,
+    jeevanmanandar,nabindawadi,purshottamkoirala,agm208201,agm208202,agm208203,agm208204,tirtharajneupane
+}
